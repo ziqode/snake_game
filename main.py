447e7ced59048afb093c4640c7d31a00e22e92c0
ziqode <93994031+ziqode@users.noreply.github.com>
2021-12-17 22:49:06 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from turtle import  Screen
-from snake import  Snake
-from food import Food
+from turtle import Screen
+from snake import Snake
+from snack import Food
 from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width=600, height= 600)
+screen.setup(width=600, height=600)
+screen.title("Snake game")
 screen.bgcolor("black")
-screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
@@ -19,7 +19,6 @@ screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 
 game_is_on = True
@@ -46,4 +45,3 @@ while game_is_on:
             scoreboard.reset()
 
 screen.exitonclick()
-
